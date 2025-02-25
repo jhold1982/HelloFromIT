75b7955bfb86b5ae625bd5e87bbdc456744e79f3
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-02-25 09:39:42 -0600

    Initial Commit

---
+++ HelloFromIT/HelloFromITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloFromITApp.swift
+//  HelloFromIT
+//
+//  Created by Justin Hold on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloFromITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
